8f81e12a40e180a4fef59e656f12ca8e77446e9e
EridansBaumanis221RDB159 <118167655+EridansBaumanis221RDB159@users.noreply.github.com>
2023-04-17 14:58:20 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -1,10 +1,18 @@
 # python3
+import hashlib
 
 def read_input():
     # this function needs to aquire input both from keyboard and file
     # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
-    
-    
+    input_type = input().rstrip()
+    if input_type == 'I':
+        pattern = input().rstrip()
+        text = input().rstrip()
+    elif input_type == 'F':
+        with open('tests/06', 'r') as file:
+            pattern = file.readline().rstrip()
+            text = file.readline().rstrip()
+    return pattern, text
     # after input type choice
     # read two lines 
     # first line is pattern 
@@ -13,7 +21,6 @@ def read_input():
     # return both lines in one return
     
     # this is the sample return, notice the rstrip function
-    return (input().rstrip(), input().rstrip())
 
 def print_occurrences(output):
     # this function should control output, it doesn't need any return
@@ -21,12 +28,21 @@ def print_occurrences(output):
 
 def get_occurrences(pattern, text):
     # this function should find the occurances using Rabin Karp alghoritm 
-
+    pattern_length = len(pattern)
+    text_length = len(text)
+    pattern_hash = hashlib.sha256(pattern.encode()).hexdigest()
+    text_hash = hashlib.sha256(text.encode()).hexdigest()
+    occurrences = []
+    for x in range(text_length - pattern_length +1):
+        if pattern_hash == hashlib.sha256(text[x:x+pattern_length].encode()).hexdigest():
+            if pattern == text[x:x+pattern_length]
+            occurrences.append(x)
+        if x < text_length - pattern_length:
+            text_hash = hashlib.sha256(text[x+1:x+pattern_length+1].encode()).hexdigest():
     # and return an iterable variable
-    return [0]
+    return occurrences
 
 
 # this part launches the functions
 if __name__ == '__main__':
     print_occurrences(get_occurrences(*read_input()))
-
